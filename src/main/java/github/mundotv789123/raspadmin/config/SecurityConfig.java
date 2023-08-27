929e8f743e4262b3381dc15a12f5cd6a9993d822
mundotv789123 <789michaelfernandes789@gmail.com>
2023-08-27 20:29:05 -0300

    Corrigindo mensagens de erros de autenticação

--- src/main/java/github/mundotv789123/raspadmin/config/SecurityConfig.java
+++ src/main/java/github/mundotv789123/raspadmin/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package github.mundotv789123.raspadmin.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,12 +10,18 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.Map;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     @Value("${application.security.disabled:false}")
     private boolean securityDisabled;
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Bean
     @Autowired
@@ -26,14 +33,33 @@ public class SecurityConfig {
         http.authorizeHttpRequests((request) ->
                 request.requestMatchers("/*", "/_next/**", "/img/**", "/api/auth/login").permitAll().anyRequest().authenticated()
         );
-        http.exceptionHandling((exceptionHandler) -> exceptionHandler.authenticationEntryPoint((request, response, ex) ->
-                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, ex.getMessage())
-        ));
+        http.exceptionHandling((exceptionHandler) ->
+                exceptionHandler.authenticationEntryPoint((request, response, ex) -> {
+                    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                    response.setContentType("application/json");
+
+                    var responseBody = new HashMap<String, String>();
+                    responseBody.put("message", ex.getMessage());
+
+                    ObjectMapper objectMapper = new ObjectMapper();
+                    response.getWriter().write(objectMapper.writeValueAsString(responseBody));
+                })
+        );
         http.formLogin((login) ->
-                login.loginProcessingUrl("/api/auth/login")
+                login.loginProcessingUrl("/api/auth/login").failureHandler((request, response, ex) -> {
+                    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                    response.setContentType("application/json");
+
+                    var responseBody = new HashMap<String, String>();
+                    responseBody.put("message", ex.getMessage());
+
+                    ObjectMapper objectMapper = new ObjectMapper();
+                    response.getWriter().write(objectMapper.writeValueAsString(responseBody));
+                }).successHandler((request, response, ex) -> {
+                    response.setStatus(HttpServletResponse.SC_OK);
+                })
         );
         http.csrf((csrf) -> csrf.disable());
         return http.build();
     }
-
 }
