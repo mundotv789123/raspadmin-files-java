ab570300bb25d589bc0ed5bd56452de75dd8cd7d
mundotv789123 <789michaelfernandes789@gmail.com>
2023-09-18 07:03:57 -0300

    Limpando testes

---
+++ src/test/java/github/mundotv789123/raspadmin/RaspadminApplicationMockTests.java
@@ -0,0 +1,104 @@
+package github.mundotv789123.raspadmin;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(properties = {
+    "application.security.disabled=false",
+    "spring.security.user.name=admin",
+    "spring.security.user.password=admin"
+})
+@AutoConfigureMockMvc
+public class RaspadminApplicationMockTests {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("Test access protected routers")
+    void accessRouters() throws Exception {
+        this.mockMvc.perform(post("/api/files?path=/")).andDo(print())
+            .andExpect(status().isUnauthorized());
+        this.mockMvc.perform(post("/api/files/open?path=/")).andDo(print())
+            .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @DisplayName("Test login request fail")
+    void loginFail() throws Exception {
+        this.mockMvc.perform(post("/api/auth/login")
+            .param("username", "admin")
+            .param("password", "admin2")
+        ).andDo(print()).andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @DisplayName("Test login request success")
+    void loginSuccess() throws Exception {
+        this.mockMvc.perform(post("/api/auth/login")
+            .param("username", "admin")
+            .param("password", "admin")
+        ).andDo(print()).andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("Test list files request")
+    @WithMockUser(username = "admin", password = "admin")
+    void testListFiles() throws Exception {
+        this.mockMvc.perform(get("/api/files?path=/"))
+            .andDo(print())
+            .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("Test list files not found request")
+    @WithMockUser(username = "admin", password = "admin")
+    void testListFilesPathNotFound() throws Exception {
+        this.mockMvc.perform(get("/api/files?path=/teste_not_found"))
+            .andDo(print())
+            .andExpect(status().isNotFound());
+    }
+
+    @Test
+    @DisplayName("Test open file request")
+    @WithMockUser(username = "admin", password = "admin")
+    void testFileOpenPath() throws Exception {
+        this.mockMvc.perform(get("/api/files/open?path=/teste/teste.txt"))
+            .andDo(print())
+            .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("Test open partial file request")
+    @WithMockUser(username = "admin", password = "admin")
+    void openPartialFileRequest() throws Exception {
+        var headers = new HttpHeaders();
+        headers.add("Range", "bytes=0-");
+        this.mockMvc
+            .perform(get("/api/files/open?path=/teste/teste.txt").headers(headers))
+            .andDo(print())
+            .andExpect(status().isPartialContent());
+    }
+
+    @Test
+    @DisplayName("Test open invalid partial file request")
+    @WithMockUser(username = "admin", password = "admin")
+    void openInvalidPartialFileRequest() throws Exception {
+        var headers = new HttpHeaders();
+        headers.add("Range", "bytes=");
+        this.mockMvc
+            .perform(get("/api/files/open?path=/teste/teste.txt").headers(headers))
+            .andDo(print())
+            .andExpect(status().isRequestedRangeNotSatisfiable());
+    }
+}
