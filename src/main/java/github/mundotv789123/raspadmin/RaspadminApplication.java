3402e5968ac15ff64d0b3020ed3dea30a45be09a
mundotv789123 <789michaelfernandes789@gmail.com>
2023-10-27 08:30:09 -0300

    SpringBootServlet

--- src/main/java/github/mundotv789123/raspadmin/RaspadminApplication.java
+++ src/main/java/github/mundotv789123/raspadmin/RaspadminApplication.java
@@ -2,9 +2,10 @@ package github.mundotv789123.raspadmin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RaspadminApplication {
+public class RaspadminApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(RaspadminApplication.class, args);
