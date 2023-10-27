ace121d69c4f06a692f366ad9be8c751b3d056dc
mundotv789123 <789michaelfernandes789@gmail.com>
2023-10-27 08:44:32 -0300

    Incluindo log no envio de arquivo

--- src/main/java/github/mundotv789123/raspadmin/services/FileStreamService.java
+++ src/main/java/github/mundotv789123/raspadmin/services/FileStreamService.java
@@ -1,6 +1,8 @@
 package github.mundotv789123.raspadmin.services;
 
 import lombok.Getter;
+import lombok.extern.log4j.Log4j2;
+
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
 import java.io.File;
@@ -8,6 +10,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 
+@Log4j2
 public class FileStreamService implements StreamingResponseBody {
 
     private final File file;
@@ -37,15 +40,19 @@ public class FileStreamService implements StreamingResponseBody {
             int length;
 
             long readed = 0;
-            while ((length = in.read(buffer)) > 0) {
-                readed += length;
-                if (end > start && (readed > maxLength)) {
-                    int len = (int) (readed - maxLength);
-                    outputStream.write(buffer, 0, len);
-                    break;
-                }
+            try {
+                while ((length = in.read(buffer)) > 0) {
+                    readed += length;
+                    if (end > start && (readed > maxLength)) {
+                        int len = (int) (readed - maxLength);
+                        outputStream.write(buffer, 0, len);
+                        break;
+                    }
 
-                outputStream.write(buffer, 0, length);
+                    outputStream.write(buffer, 0, length);
+                }
+            } catch (Exception ex) {
+                log.error("Error while sending file", ex);
             }
         }
     }
