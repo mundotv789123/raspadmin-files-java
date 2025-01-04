715e129dcefc949b4c8b8a4ccaf37b5520c6b871
Michael Fernandes <789michaelfernandes789@gmail.com>
2025-01-04 14:06:15 -0300

    ajustes logs, ip e organização por pastas

--- src/main/java/github/mundotv789123/raspadmin/filters/LoggerIpAddressFilter.java
+++ src/main/java/github/mundotv789123/raspadmin/filters/LoggerIpAddressFilter.java
@@ -1,7 +1,10 @@
 package github.mundotv789123.raspadmin.filters;
 
 import java.io.IOException;
+import java.util.Arrays;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.web.util.matcher.IpAddressMatcher;
 import org.springframework.stereotype.Component;
 
 import jakarta.servlet.Filter;
@@ -13,11 +16,31 @@ import jakarta.servlet.http.HttpServletRequest;
 
 @Component
 public class LoggerIpAddressFilter implements Filter {
+
+    @Value("${application.trust.ips:''}")
+    private String trustIps;
+
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest httpRequest = (HttpServletRequest) request;
-        String ipAddress = httpRequest.getRemoteAddr();
+
+        String remoteAddress = httpRequest.getRemoteAddr();
+        String ipAddress = remoteAddress;
+
+        String ipAddressHeader = httpRequest.getHeader("X-Forwarded-For");
+        if (ipAddressHeader != null) {
+            var trustIpsSplited = Arrays.asList(trustIps.split(","));
+            if (trustIpsSplited.stream().filter(ip -> isIpInRange(remoteAddress, ip)).count() > 0) {
+                ipAddress = ipAddressHeader;
+            }
+        }
+
         org.apache.logging.log4j.ThreadContext.put("ipAddress", ipAddress);
         chain.doFilter(request, response);
+    }
+
+    private boolean isIpInRange(String ipAddress, String cidr) {
+        IpAddressMatcher ipMatcher = new IpAddressMatcher(cidr);
+        return ipMatcher.matches(ipAddress);
     }
 }
